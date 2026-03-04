0757f387165c33ac7f36c9f3dcf1200697fa9b0c
sana968sana-pixel <sana968sana@gmail.com>
2026-03-04 20:26:01 +0500

    Create part5.py
    assignment no 1 part 5

---
+++ part5.py
@@ -0,0 +1,50 @@
+# Initialize a variable x = 10. Use += to add 5 to it.
+x=10
+x+=5
+print(x)
+
+# . Use -= to subtract 3 from a variable price
+price=30
+price-=3
+print(price)
+
+# . Multiply a variable balance by 2 using the *= operator.
+balance=2500
+balance*=2
+print(balance)
+
+# . Divide a variable total by 4 using the /= operator
+total=400
+total/=4
+print(total)
+
+# Use **= to square a variable.
+square=2
+square**=square
+print(square)
+
+# Create a counter variable and increment it by 1 using assignment operators
+counter=4
+counter+=1
+print(counter)
+
+# . Use %= to find the remainder of a variable divided by 2 and update the variable.
+var=50
+var%=2
+print(var)
+
+# Use //= to perform floor division on a variable and update it.
+num=5
+num//=2
+print(num)
+
+# Start with n = 2. In three lines of code using assignment operators, turn it into 20.
+n=2
+n*=5
+n*=2
+print(n)
+
+# Ask the user for a number, then use += to add 10 to it and print the final result.
+number=int(input("enter number:"))
+number+=10
+print(number)
